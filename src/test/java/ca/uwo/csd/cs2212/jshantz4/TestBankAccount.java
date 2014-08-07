7973c6fb87ee06b5c08b44c4da35a12b849b19df
Mohammed Faryaz <moinuddin_m@Mohammed-M.India.XoriantCorp.com>
2014-08-07 15:12:57 +0530

    updated

--- src/test/java/ca/uwo/csd/cs2212/jshantz4/TestBankAccount.java
+++ src/test/java/ca/uwo/csd/cs2212/jshantz4/TestBankAccount.java
@@ -3,12 +3,6 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-Public class TestBankAccount 
-{
-@Test
-public void testDebitWithSufficientFunds(){
-BankAccount account = new BankAccount(10);
-double amount = account.debit(5);
-Assert.assertEquals(5.0, amount);
-}
+public class TestBankAccount {
+
 }
